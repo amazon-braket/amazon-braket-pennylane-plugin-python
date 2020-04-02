234661f25211004906c39070ddd287c750a0db97
Cody Wang <speller26@gmail.com>
2020-04-02 13:18:30 -0600

    Improve example usage (#8)

--- example.py
+++ example.py
@@ -1,25 +1,39 @@
 import pennylane as qml
 
-bucket = ("braket-output-355401967590", "test1")
+s3 = ("my-bucket", "my-prefix")
 
-dev1 = qml.device("braket.simulator", wires=2, s3_destination_folder=bucket)
-dev2 = qml.device("braket.simulator", wires=2, backend="QS3", s3_destination_folder=bucket)
+dev_qs2 = qml.device(
+	"braket.simulator",
+	s3_destination_folder=s3,
+	backend="QS2",
+	wires=2
+)
+dev_qpu = qml.device(
+	"braket.rigetti",
+	s3_destination_folder=s3,
+	poll_timeout_seconds=1800,
+	shots=10000,
+	wires=2
+)
 
-@qml.qnode(dev1)
+
+@qml.qnode(dev_qs2)
 def circuit(a):
 	qml.Hadamard(wires=0)
 	qml.CNOT(wires=[0, 1])
 	qml.RX(a, wires=1)
 	return qml.expval(qml.PauliZ(1))
+
 
 print(circuit(0.543))
 
 
-@qml.qnode(dev2)
+@qml.qnode(dev_qpu)
 def circuit(a):
 	qml.Hadamard(wires=0)
 	qml.CNOT(wires=[0, 1])
 	qml.RX(a, wires=1)
 	return qml.expval(qml.PauliZ(1))
+
 
 print(circuit(0.543))
