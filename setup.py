0a779e889132eced2066127e3de5a9ad2c2e32fb
trbromley <brotho02@gmail.com>
2020-10-20 09:21:40 -0400

    Iterate PL version number

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     python_requires=">= 3.7.2",
     packages=find_namespace_packages(where="src", exclude=("test",)),
     package_dir={"": "src"},
-    install_requires=["amazon-braket-sdk", "pennylane"],
+    install_requires=["amazon-braket-sdk", "pennylane>=0.12"],
     entry_points={
         "pennylane.plugins": [
             # List the short name of each device provided by
