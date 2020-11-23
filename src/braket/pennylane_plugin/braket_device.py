0484bba48e83d08c7bb451003df6792291eb450a
Lin <licedric@amazon.com>
2020-11-23 00:35:30 -0800

    Refactor translation from Braket to PennyLane results to read task results directly instead of Braket tasks

--- src/braket/pennylane_plugin/braket_device.py
+++ src/braket/pennylane_plugin/braket_device.py
@@ -121,12 +121,12 @@ class BraketQubitDevice(QubitDevice):
         return braket_circuit
 
     def statistics(
-        self, task: GateModelQuantumTaskResult, observables: Sequence[Observable]
+        self, braket_result: GateModelQuantumTaskResult, observables: Sequence[Observable]
     ) -> Union[float, List[float]]:
-        """Process measurement results from a Braket task and return statistics.
+        """Processes measurement results from a Braket task result and returns statistics.
 
         Args:
-            task (GateModelQuantumTaskResult): the executed task
+            braket_result (GateModelQuantumTaskResult): the Braket task result
             observables (List[Observable]): the observables to be measured
 
         Raises:
@@ -141,15 +141,15 @@ class BraketQubitDevice(QubitDevice):
                 raise QuantumFunctionError(
                     "Unsupported return type specified for observable {}".format(obs.name)
                 )
-            results.append(self._get_statistic(task, obs))
+            results.append(self._get_statistic(braket_result, obs))
 
         return results
 
-    def _task_to_results(self, task, circuit):
-        """Calculates the results from a Braket task. A PennyLane circuit also
-        determines the output observables."""
+    def _braket_to_pl_result(self, braket_result, circuit):
+        """Calculates the PennyLane results from a Braket task result. A PennyLane circuit
+        also determines the output observables."""
         # Compute the required statistics
-        results = self.statistics(task, circuit.observables)
+        results = self.statistics(braket_result, circuit.observables)
 
         # Ensures that a combination with sample does not put
         # single-number results in superfluous arrays
@@ -163,7 +163,7 @@ class BraketQubitDevice(QubitDevice):
         self.check_validity(circuit.operations, circuit.observables)
         self._circuit = self._pl_to_braket_circuit(circuit, **run_kwargs)
         self._task = self._run_task(self._circuit)
-        return self._task_to_results(self._task, circuit)
+        return self._braket_to_pl_result(self._task.result(), circuit)
 
     def apply(
         self, operations: Sequence[Operation], rotations: Sequence[Operation] = None, **run_kwargs
@@ -195,8 +195,8 @@ class BraketQubitDevice(QubitDevice):
         raise NotImplementedError("Need to implement task runner")
 
     @staticmethod
-    def _get_statistic(task, observable):
-        return task.result().get_value_by_result_type(translate_result_type(observable))
+    def _get_statistic(braket_result, observable):
+        return braket_result.get_value_by_result_type(translate_result_type(observable))
 
 
 class BraketAwsQubitDevice(BraketQubitDevice):
@@ -300,12 +300,11 @@ class BraketAwsQubitDevice(BraketQubitDevice):
             **self._run_kwargs,
         )
         # Call results() to retrieve the Braket results in parallel.
-        task_batch.results()
-        results = [
-            self._task_to_results(task, circuit)
-            for task, circuit in zip(task_batch.tasks, circuits)
+        braket_results_batch = task_batch.results()
+        return [
+            self._braket_to_pl_result(braket_result, circuit)
+            for braket_result, circuit in zip(braket_results_batch, circuits)
         ]
-        return results
 
     def _run_task(self, circuit):
         return self._device.run(
