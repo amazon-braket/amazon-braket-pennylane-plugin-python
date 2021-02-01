40ed380df362f7387bcd8da116747b57250f0644
trbromley <brotho02@gmail.com>
2021-02-01 09:27:11 -0500

    Add unit test

--- test/unit_tests/test_ops.py
+++ test/unit_tests/test_ops.py
@@ -15,7 +15,14 @@ import math
 
 import numpy as np
 import pytest
-from braket.circuits import gates
+from braket.circuits import gates, observables
+from braket.circuits.result_types import Expectation, Probability, Sample, Variance
+import pennylane as qml
+from braket.pennylane_plugin.translation import translate_result_type
+
+from pennylane.tape.measure import MeasurementProcess
+from pennylane.operation import ObservableReturnTypes
+from pennylane.wires import Wires
 
 from braket.pennylane_plugin import (
     ISWAP,
@@ -48,3 +55,38 @@ testdata = [
 def test_matrices(pl_op, braket_gate, params):
     """Tests that the matrices of the custom operations are correct."""
     assert np.allclose(pl_op._matrix(*params), braket_gate(*params).to_matrix())
+
+
+pl_return_types = [
+    ObservableReturnTypes.Expectation,
+    ObservableReturnTypes.Variance,
+    ObservableReturnTypes.Sample,
+]
+
+braket_results = [
+    Expectation(observables.H(), [0]),
+    Variance(observables.H(), [0]),
+    Sample(observables.H(), [0]),
+]
+
+
+@pytest.mark.parametrize("return_type, braket_result", zip(pl_return_types, braket_results))
+def test_translate_result_type_observable(return_type, braket_result):
+    """Tests if a PennyLane return type that involves an observable is successfully converted into a
+    Braket result"""
+    obs = qml.Hadamard(0)
+    obs.return_type = return_type
+    braket_result_calculated = translate_result_type(obs)
+
+    assert braket_result == braket_result_calculated
+
+
+def test_translate_result_type_probs():
+    """Tests if a PennyLane probability return type is successfully converted into a Braket
+    result."""
+    mp = MeasurementProcess(ObservableReturnTypes.Probability, wires=Wires([0]))
+    braket_result_calculated = translate_result_type(mp)
+
+    braket_result = Probability([0])
+
+    assert braket_result == braket_result_calculated
