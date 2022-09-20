0c951e0fe3869aff802739081ed6858c5b120cb5
Aaron Berdy <berdy@amazon.com>
2022-09-20 13:03:05 -0700

    feature: add identity operation (#109)

--- src/braket/pennylane_plugin/translation.py
+++ src/braket/pennylane_plugin/translation.py
@@ -41,6 +41,7 @@ from braket.pennylane_plugin.ops import (
 )
 
 _BRAKET_TO_PENNYLANE_OPERATIONS = {
+    "i": "Identity",
     "x": "PauliX",
     "y": "PauliY",
     "z": "PauliZ",
@@ -126,6 +127,11 @@ def _translate_operation(operation: Operation, _parameters) -> Gate:
     raise NotImplementedError(
         f"Braket PennyLane plugin does not support operation {operation.name}."
     )
+
+
+@_translate_operation.register
+def _(_: qml.Identity, _parameters):
+    return gates.I()
 
 
 @_translate_operation.register
