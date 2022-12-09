d1de23fce914bd45589de1e8cb85ca5e3bda5348
Jacob Feldman <jacofeld@amazon.com>
2022-12-09 11:05:12 -0800

    fixing linters

--- test/integ_tests/test_adjoint_gradient.py
+++ test/integ_tests/test_adjoint_gradient.py
@@ -15,14 +15,11 @@
 """Tests that gradients are correctly computed in the plugin device via braket"""
 
 import math
-import os
 import random
-import warnings
 
 import pennylane as qml
 import pytest
 from pennylane import numpy as np
-from pennylane import qchem
 
 ABS_TOLERANCE = 1e-5
 
