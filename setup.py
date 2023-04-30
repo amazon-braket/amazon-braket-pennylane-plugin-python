ae47fa82a6ce8cb004a9b78d663f5462b575c4a5
lillian542 <38584660+lillian542@users.noreply.github.com>
2023-04-30 17:56:57 -0400

    Expval can handle NaN values (#157)
    Co-authored-by: Mudit Pandey <mudit.pandey@xanadu.ai>
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    Co-authored-by: Cody Wang <speller26@gmail.com>

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
+import platform
+
 from setuptools import find_namespace_packages, setup
 
 with open("README.rst", "r") as fh:
@@ -18,6 +20,11 @@ with open("README.rst", "r") as fh:
 
 with open("src/braket/pennylane_plugin/_version.py") as f:
     version = f.readlines()[-1].split()[-1].strip("\"'")
+
+if platform.system() == "Darwin" and platform.machine() == "arm64":
+    TF_VERSION = "tensorflow-macos>=2.12.0"
+else:
+    TF_VERSION = "tensorflow>=2.12.0"
 
 setup(
     name="amazon-braket-pennylane-plugin",
@@ -62,8 +69,8 @@ setup(
             "sphinx-rtd-theme",
             "sphinxcontrib-apidoc",
             "tox",
-            "tensorflow>=2.12.0",
             "torch>=1.11",
+            TF_VERSION,
         ]
     },
     url="https://github.com/aws/amazon-braket-pennylane-plugin-python",
