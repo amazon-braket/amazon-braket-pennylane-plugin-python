d52b5e4a7369decf01696bfd28e2d6e0e102abe8
Cody Wang <speller26@gmail.com>
2023-10-20 17:32:51 -0700

    depr: Remove legacy return types system (#211)

--- src/braket/pennylane_plugin/braket_device.py
+++ src/braket/pennylane_plugin/braket_device.py
@@ -746,10 +746,12 @@ class BraketAwsQubitDevice(BraketQubitDevice):
             else:
                 results = self.execute(circuit, compute_gradient=True)
                 new_res, new_jac = results
+                # PennyLane expects the forward execution result to be a scalar
+                # when it is accompanied by an adjoint gradient calculation
+                new_res = new_res[0]
                 new_jac = self._adjoint_jacobian_processing(new_jac)
             res.append(new_res)
             jacs.append(new_jac)
-        res = res[0] if len(res) == 1 else res
         return res, jacs
 
 
