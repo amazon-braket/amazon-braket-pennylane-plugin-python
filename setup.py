a55a943b2d93b1d296888903d27e9f9ad732b630
Abe Coull <85974725+math411@users.noreply.github.com>
2024-03-27 17:31:57 -0700

    infra: add lower constraint to pylint

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
             "flake8-rst-docstrings",
             "isort",
             "pre-commit",
-            "pylint",
+            "pylint>=3.1",
             "pytest",
             "pytest-cov",
             "pytest-mock",
