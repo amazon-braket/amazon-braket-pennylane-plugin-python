7583d98595a0ae75a7d6ae920f3997c8b91d6c20
Abe Coull <85974725+math411@users.noreply.github.com>
2024-10-23 11:51:56 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     },
     extras_require={
         "test": [
-            "autoray<0.7.0" # autoray.tensorflow_diag no longer works
+            "autoray<0.7.0", # autoray.tensorflow_diag no longer works
             "docutils>=0.19",
             "flaky",
             "pre-commit",
