9bedc13412169141bc1c36e298b4ae38f13e7698
Cody Wang <speller26@gmail.com>
2026-09-16 14:17:32 -0700

    Suppress lint warning

--- src/braket/pennylane_plugin/braket_device.py
+++ src/braket/pennylane_plugin/braket_device.py
@@ -973,7 +973,7 @@ class BraketAwsQubitDevice(BraketQubitDevice):
             if isinstance(op, qp.pulse.ParametrizedEvolution):
                 self._validate_pulse_parameters(op)
 
-    def capabilities(self=None):
+    def capabilities(self=None):  # noqa: RUF077
         """Add support for AG on sv1"""
         # normally, we'd just call super().capabilities() here, but super()
         # resolution doesn't work when you override a classmethod with an instance method
